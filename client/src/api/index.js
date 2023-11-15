83fc0328f3d2ae035eeaf2e2939fa3db75309b6f
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-11-15 15:39:38 +0530

    starter

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API=axios.create({baseURL:"http://localhost:5000"});
+// const API=axios.create({baseURL:"http://localhost:5000"});
+const API=axios.create({baseURL:"https://carmarketserver.onrender.com/"});
 
 
 
